e3c3a9dafdfa19285d29272f819b943d8700f57f
Matt <mwbertolucci@gmail.com>
2023-05-03 17:13:44 -0400

    Add owners tab,species tab and modify animals

--- schema.sql
+++ schema.sql
@@ -10,3 +10,32 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(30);
+
+
+-- Create a table called owners with the following columns:
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(30),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+-- Create a table called species with the following columns:
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+-- Modify animals table as follows:
+-- Set id as auto incremented primary key.
+ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+
+-- Remove the species column.
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add a column called species_id which references the id column of the species table.
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+
+-- Add a column called owner_id which references the id column of the owners table.
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
