f43b95535d88c1e70a42aa8be81d4c52cf4fa4f7
Matt <mwbertolucci@gmail.com>
2023-05-05 18:41:04 -0400

    Create join tables

--- schema.sql
+++ schema.sql
@@ -39,3 +39,28 @@ ALTER TABLE animals ADD species_id INT REFERENCES species(id);
 
 -- Add a column called owner_id which references the id column of the owners table.
 ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
+-- Create a table called vets with the following columns:
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(30),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
+
+-- Create a join table called specializations with the following columns:
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  vet_id INT REFERENCES vets(id),
+  species_id INT REFERENCES species(id),
+  PRIMARY KEY(id)
+);
+
+-- Create a join table called visits with the following columns:
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
+);
